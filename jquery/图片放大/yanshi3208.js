7f4d0b69028feb2ab05cd9d5ef3a98fa1c90151b
wunult <wunult@163.com>
2019-08-28 17:18:01 +0800

    0828first
    first require

---
+++ jquery/图片放大/yanshi3208.js
@@ -0,0 +1,1 @@
+var i=this.id,s=window.google_iframe_oncopy,H=s&&s.handlers,h=H&&H[i],w=this.contentWindow,d;try{d=w.document}catch(e){}if(h&&d&&(!d.body||!d.body.firstChild)){if(h.call){setTimeout(h,0)}else if(h.match){try{h=s.upd(h,i)}catch(e){}w.location.replace(h)}}
